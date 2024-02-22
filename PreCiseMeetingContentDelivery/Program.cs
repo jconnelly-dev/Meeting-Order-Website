8e86b9c622d8e3cc912980deb8ccc514ce8138fe
jconnelly-dev <53546309+jconnelly-dev@users.noreply.github.com>
2024-02-21 21:58:21 -0800

    beautiful - the site is up and running :)!

--- PreCiseMeetingContentDelivery/Program.cs
+++ PreCiseMeetingContentDelivery/Program.cs
@@ -4,15 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 var app = builder.Build();
 
-//if (!builder.Environment.IsDevelopment())
-//{
-//    builder.Services.AddHttpsRedirection(options =>
-//    {
-//        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
-//        options.HttpsPort = 443;
-//    });
-//}
-
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -22,7 +13,6 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-//app.MapShortCircuit(404, "robots.txt");
 app.MapRazorPages();
 
 app.Run();
