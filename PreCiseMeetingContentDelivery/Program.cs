3c97cbfdc22671980fc018048b542e42daad9795
jconnelly-dev <53546309+jconnelly-dev@users.noreply.github.com>
2024-02-21 21:56:01 -0800

    build was successful, but got a Heroku H10 error 'A crashed web dyno or a boot timeout on the web dyno will present this error.'

--- PreCiseMeetingContentDelivery/Program.cs
+++ PreCiseMeetingContentDelivery/Program.cs
@@ -4,14 +4,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 var app = builder.Build();
 
-if (!builder.Environment.IsDevelopment())
-{
-    builder.Services.AddHttpsRedirection(options =>
-    {
-        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
-        options.HttpsPort = 443;
-    });
-}
+//if (!builder.Environment.IsDevelopment())
+//{
+//    builder.Services.AddHttpsRedirection(options =>
+//    {
+//        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+//        options.HttpsPort = 443;
+//    });
+//}
 
 if (!app.Environment.IsDevelopment())
 {
@@ -22,7 +22,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-app.MapShortCircuit(404, "robots.txt");
+//app.MapShortCircuit(404, "robots.txt");
 app.MapRazorPages();
 
 app.Run();
